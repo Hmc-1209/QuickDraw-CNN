44b7bff00dea0e6029768c6c1206b2f2cb297d7f
DannyHo1209 <113899395+DannyHo1209@users.noreply.github.com>
2022-11-23 23:27:47 +0800

    testforgitignore

---
+++ dataset/generateDatas.py
@@ -0,0 +1,28 @@
+"""
+    To generate datas from quickdraw database, simply call generateData
+    function and pass the keywords and values in, then the code will
+    automatically generate pictures in this folder.
+
+    .quickdrawcache is a cache when calling quickdraw library.
+    You don't need to do anything about it.
+"""
+
+from quickdraw import QuickDrawDataGroup
+
+
+def generateData(key, value):
+    # Generate datas from quickdraw libraries
+    keys = QuickDrawDataGroup(key, max_drawings=value)
+    # Integer i for sequential file name
+    i = 0
+    for element in keys.drawings:
+        # Saving pictures
+        element.image.save(key + str(i) + '.png')
+        i += 1
+
+
+"""
+Generate data using the below form 
+generateData('anvil', 100) --> # Representing generate 100 anvil pictures a
+"""
+
