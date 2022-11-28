c420eaa7efc8cdc493c33e51b785c12a5bb4afa5
DannyHo1209 <113899395+DannyHo1209@users.noreply.github.com>
2022-11-28 10:54:05 +0800

    loadData-2

--- loadData.py
+++ loadData.py
@@ -1,9 +1,15 @@
 """
     This is a code that been used by training and testing models
+    It would automatically download the .npy files needed, if the
+    code doesn't work for some reason, please download the files
+    from the given link in README.md
 """
 
 import numpy as np
 
+# Datasets
+keys = ['cat', 'diamond', 'eye', 'ladder', 'moon']
+# keys = ['cat', 'diamond', 'eye', 'ladder', 'moon', 'necklace', 'snowflake', 'sword', 'tornado', 'wine glass']
 
 # Split the list
 def split_list(ls, n):
@@ -15,9 +21,44 @@ def split_list(ls, n):
 
 # Function for loading data
 def load(key):
-    rawDatas = np.load('./dataset/full_numpy_bitmap_'+key+'.npy')
+    # If the dataset does not exist, catch the error
+    try:
+        rawDatas = np.load('./dataset/full_numpy_bitmap_'+key+'.npy')
+    except:
+        print('Failed to get full_numpy_bitmap_' + key + '.npy in dataset folder.')
+
     data = []
     for rawData in rawDatas[:10000]:
         data.append(split_list(rawData, 28))
 
     return data
+
+# Functions to be called for getting datas
+def loadDatas():
+    # Getting datas
+    train_data = []
+    train_label = []
+    test_data = []
+    test_label = []
+    # Variable types for categorical uses
+    types = 0
+    # Downloading datas from internet
+
+    print('Loading datas ... ')
+    for key in keys:
+        # Calling load() from loadData.py
+        datas = load(key)
+        # Split the data into train and test data
+        train_data += datas[:8000]
+        test_data += datas[8000:]
+        # Generate labels for train and test data
+        for i in range(8000):
+            train_label.append(types)
+        for i in range(2000):
+            test_label.append(types)
+        datas.clear()
+        types += 1
+    print('Loading complete !')
+    print('There are ' + str(types) + ' types of images loaded :')
+    print(keys)
+    return train_data, train_label, test_data, test_label
